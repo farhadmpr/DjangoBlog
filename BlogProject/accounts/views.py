3d05997e6f942184a02e91084dca7332cdada09c
farhadmpr <farhadmp@gmail.com>
2019-12-28 09:43:02 +0330

    Fix account register bug

--- BlogProject/accounts/views.py
+++ BlogProject/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 # Create your views here.
 
