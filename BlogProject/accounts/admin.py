d9a464e38355ee8ae327ab98e4150d9ebca75be5
farhadmpr <farhadmp@gmail.com>
2020-01-04 09:58:56 +0330

    Add profile to admin page

--- BlogProject/accounts/admin.py
+++ BlogProject/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class ExtendedProfileAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedProfileAdmin)
+
